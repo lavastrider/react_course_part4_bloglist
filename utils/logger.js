7460d6f568319d028138208ca1179cc6416a1ae6
lavastrider <70551407+lavastrider@users.noreply.github.com>
2023-03-09 18:19:12 -0600

    setup

---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+const config = require('.config')
+
+const info = (...params) => {
+	console.log(...params)
+}
+
+const error = (...params) => {
+	console.error(...params)
+}
+
+module.exports = {
+	info, error
+}
